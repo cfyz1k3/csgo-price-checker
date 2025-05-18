15a63c546d9cceb0def7eb5baf1551f5c7ab291a
Sanya <cmcfuz@gmail.com>
2025-05-18 23:35:47 +0300

    Добавлен маршрут /api/youpin_names и обновлён раздел Potok

--- app.py
+++ app.py
@@ -118,3 +118,7 @@ def serve_static_file(filename):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+@app.route("/api/youpin_names")
+def youpin_skin_names():
+    return jsonify(list(find_id.keys()))
