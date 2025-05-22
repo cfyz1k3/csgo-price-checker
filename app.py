67449d4df4a5d3a7368dc8348c7956a8ce759a90
Sanya <cmcfuz@gmail.com>
2025-05-22 15:58:27 +0300

    Обновления: удалены тестовые карточки и обновлён app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def get_price_data():
         results["buff"] = None
 
     try:
-        r = requests.get(f"https://market.csgo.com/api/v2/search-item-by-hash-name?key=MuvCYEV99pjqEkV0505dC1h9q33OdaG&hash_name={name}", timeout=10)
+        r = requests.get(f"https://market.csgo.com/api/v2/search-item-by-hash-name?key=v2866i5cp2ZmX6M12yJISnRdRuNbZ40&hash_name={name}", timeout=10)
         data = r.json()
         results["leg1t"] = float(data["data"][0]["price"]) if "data" in data else None
     except:
